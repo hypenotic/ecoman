ca5a8393e629079f66cb4a6d38caa39e56a17446
Emily Dela Cruz <delacruz.emily@gmail.com>
2016-01-25 09:33:51 -0500

    setup

---
+++ custom/themes/ecoman/functions.php
@@ -0,0 +1,35 @@
+<?php
+include('includes/wp-cuztom-helper/cuztom.php');
+//Include post custom posts type. Dependent on /wp-cuztom-helper classes.
+include('includes/wp-cuztom-posts/custom-generic.php');
+include('includes/wp-cuztom-posts/custom-generic-post.php');
+
+
+//Load custom functions
+require_once('includes/functions/enqueue-style.php');
+require_once('includes/functions/enqueue-script.php');
+require_once('includes/functions/cuztom-posts.php');
+
+//Load shortcodes
+
+add_theme_support( 'post-thumbnails' ); 
+
+// Async/Defer Scripts
+/*function to add async to all scripts*/
+function js_async_attr($tag){
+
+# Add async to all remaining scripts
+return str_replace( ' src', ' defer src', $tag );
+}
+add_filter( 'script_loader_tag', 'js_async_attr', 10 );
+
+/*** Remove Query String from Static Resources ***/
+function remove_cssjs_ver( $src ) {
+ if( strpos( $src, '?ver=' ) )
+ $src = remove_query_arg( 'ver', $src );
+ return $src;
+}
+add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
+add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
+
+?>
