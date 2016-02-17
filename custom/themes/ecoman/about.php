da69f42701c4730d1ac00c63aa1c04bb73b88e70
Emily Dela Cruz <delacruz.emily@gmail.com>
2016-02-17 17:32:56 -0500

    Update: about

--- custom/themes/ecoman/about.php
+++ custom/themes/ecoman/about.php
@@ -1,16 +1,52 @@
 <?php /* Template Name: About */ ?>
 
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
 <?php get_header(); ?>	
 
-<div class="outer-container">
-	<div class="main-content">
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="tab-section-wrapper">
+	<div class="front-page__circle-icon tab-two">
 			
-			<?php the_content(); ?>
-
-		<?php endwhile; endif; ?>
 	</div>
+	<section class="front-page__intro">
+		<div class="outer-container front-page">
+			<div class="main-content">
+				<?php the_content(); ?>
+			</div>
+		</div>
+	</section>
 </div>
+
+<section class="about__services-cta">
+	
+</section>
+
+<div class="tab-section-wrapper two">
+	<div class="front-page__circle-icon tab-one">
+				
+	</div>
+	<section class="front-page__buckets about">
+		<div class="outer-container">
+			<h3>Meet Your Team</h3>
+			<div class="column-4 column-container">
+				<div class="column-4__single">
+					
+				</div>
+				<div class="column-4__single">
+					
+				</div>
+				<div class="column-4__single">
+					
+				</div>
+				<div class="column-4__single">
+					
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+
+<?php endwhile; endif; ?>
 
 <?php get_template_part( 'template-part-contact-form' ); ?>
 
