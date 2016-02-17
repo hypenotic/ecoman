91324b270f32d63150d6bfb332912de30ebb17d7
Emily Dela Cruz <delacruz.emily@gmail.com>
2016-02-17 16:13:27 -0500

    Update: general styles

--- custom/themes/ecoman/template-part-contact-form.php
+++ custom/themes/ecoman/template-part-contact-form.php
@@ -5,15 +5,15 @@
 	<div class="outer-container">
 		<div class="contact-section">
 			<div class="contact-intro">
-				<p>We love hearing from you</p>
+				<h3>We love hearing from you!</h3>
 				<?php dynamic_sidebar( 'contact-blurb' ); ?>
 			</div>
 			<div class="contact-form">
 				<div class="contact-form__left">
 					<div class="contact__deats">
-						<p>Jonas Spring</p>
+						<p>ecoman</p>
 						<p>44 shanly street,<br/>
-						Toronto, ON<br/>
+						toronto, on<br/>
 						M6H 1S3</p>
 					</div>
 					<div class="contact__blurb">
@@ -24,8 +24,8 @@
 						<ul class="social-share">
 			                	<li><a id="facebook" href="https://www.facebook.com/Ecomantoronto" target="_blank"><i class="fa fa-facebook"></i></a></li>
 			                	<li><a id="twitter" href="https://twitter.com/#!/ecomandotca" target="_blank"><i class="fa fa-twitter"></i></a></li>
-			                	<li><a id="mail" href="/" rel="nofollow" target="_blank"><i class="fa fa-envelope-o"></i></i></a></li>
-			                	<li><a id="pinterest" href="/" target="_blank"><i class="fa fa-pinterest"></i></a></li>
+			                	<li><a id="instagram" href="https://www.instagram.com/ecoman_jonas/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+			                	<li><a id="mail" href="mailto:jonas@ecoman.ca" rel="nofollow" target="_blank"><i class="fa fa-envelope-o"></i></i></a></li>
 			                </ul>
 					</div>
 				</div>
