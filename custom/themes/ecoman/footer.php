16bfe0c4b6a430409301cae956b467b992fa5993
Emily Dela Cruz <delacruz.emily@gmail.com>
2016-03-01 14:33:57 -0500

    Update: mobile menu structure

--- custom/themes/ecoman/footer.php
+++ custom/themes/ecoman/footer.php
@@ -7,12 +7,22 @@
 	<div class="mobile-nav">
 	  <?php 
 	      wp_nav_menu(array(
-	        'menu' => 'Main Menu',  
-	        'container_id' => 'main-menu',
+	        'menu' => 'Mobile Menu',  
+	        'container_id' => 'short-menu',
 	        'walker' => new Main_Menu_Walker()
 	      )); 
 	  ?>
 	  <?php get_search_form(); ?>
+	  <div class="contact__deats">
+	  	<p>ecoman</p>
+	  	<p>44 shanly street,<br/>
+	  	toronto, on<br/>
+	  	M6H 1S3</p>
+	  </div>
+	  <div class="contact__blurb">
+	  	<p>jonas@ecoman.ca<br/>
+	  	416.556.5516</p>
+	  </div>
 	</div>
 
 	<?php wp_footer(); ?>
