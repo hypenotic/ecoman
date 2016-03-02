707fb8c58a1920d05fa56bffbcdab74a7358d1b7
Emily Dela Cruz <delacruz.emily@gmail.com>
2016-03-02 12:02:06 -0500

    Update: font awesome enqueue fix

--- custom/themes/ecoman/includes/functions/enqueue-style.php
+++ custom/themes/ecoman/includes/functions/enqueue-style.php
@@ -7,6 +7,8 @@
 function my_styles() {
 	wp_register_style('flickity', 'https://npmcdn.com/flickity@1.1/dist/flickity.min.css');
  	wp_enqueue_style( 'flickity' );
+ 	wp_register_style('awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css');
+ 	wp_enqueue_style( 'awesome' );
 	wp_register_style('style', get_template_directory_uri() . '/dist/css/style.css');
  	wp_enqueue_style( 'style' );
 }
