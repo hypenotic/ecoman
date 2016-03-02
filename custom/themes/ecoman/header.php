73355216a8550c41f0ddfa71b9ec44e2596d1642
Emily Dela Cruz <delacruz.emily@gmail.com>
2016-03-02 11:22:39 -0500

    Update: GA fix, typekit flash fix

--- custom/themes/ecoman/header.php
+++ custom/themes/ecoman/header.php
@@ -16,9 +16,17 @@
 	    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 	  })(document);
 	</script>
-	<link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700|Open+Sans:400,300,600,700,700italic,600italic,400italic,300italic' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<!-- Google Analytics -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-73665508-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 	<!-- FB -->
 	<div id="fb-root"></div>
 	<script>(function(d, s, id) {
@@ -66,7 +74,7 @@
 					<a href="<?php echo home_url(); ?>">
 						<img src="<?php echo get_template_directory_uri (); ?>/dist/images/ecoman_logo_icon_black.png" alt="">
 					</a>
-					<p>ECOMAN<span class="text-disappear">. 123.456.7890</span></p>
+					<p>ECOMAN<span class="text-disappear">. 416.556.5516</span></p>
 				</div>
 				<div class="nav__right desktop">
 					<?php 
@@ -121,9 +129,9 @@
 						<?php } ?>
 					</a>
 					<?php if ($theme == 'value2') { ?>
-					<p class="light-theme">ECOMAN<span class="text-disappear">. 123.456.7890</span></p>
+					<p class="light-theme">ECOMAN<span class="text-disappear">. 416.556.5516</span></p>
 					<?php } else { ?>
-					<p>ECOMAN<span class="text-disappear">. 123.456.7890</span></p>
+					<p>ECOMAN<span class="text-disappear">. 416.556.5516</span></p>
 					<?php } ?>
 				</div>
 				<div class="nav__right desktop">
@@ -177,7 +185,7 @@
 			<nav class="desktop">
 				<div class="nav__left desktop">
 					<a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri (); ?>/dist/images/ecoman_logo_icon_white.png" alt=""></a>
-					<p class="light-theme">ECOMAN<span class="text-disappear">. 123.456.7890</span></p>
+					<p class="light-theme">ECOMAN<span class="text-disappear">. 416.556.5516</span></p>
 				</div>
 				<div class="nav__right desktop">
 					<?php 
@@ -217,7 +225,7 @@
 					<a href="<?php echo home_url(); ?>">
 						<img src="<?php echo get_template_directory_uri (); ?>/dist/images/ecoman_logo_icon_black.png" alt="">
 					</a>
-					<p>ECOMAN<span class="text-disappear">. 123.456.7890</span></p>
+					<p>ECOMAN<span class="text-disappear">. 416.556.5516</span></p>
 				</div>
 				<div class="nav__right desktop">
 					<?php //get_search_form(); ?>
