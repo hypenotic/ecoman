def739b31e8e1cc0d46f6e62922a3b76b1e5cd3d
Emily Dela Cruz <delacruz.emily@gmail.com>
2016-03-08 15:41:46 -0500

    Update: inline tweet shortcode

--- custom/themes/ecoman/functions.php
+++ custom/themes/ecoman/functions.php
@@ -30,6 +30,7 @@ require_once('includes/shortcodes/bigcopy.php');
 require_once('includes/shortcodes/bigvid.php');
 require_once('includes/shortcodes/tooltip.php');
 require_once('includes/shortcodes/vid-picture.php');
+require_once('includes/shortcodes/inline-tweet.php');
 
 
 add_theme_support( 'post-thumbnails' ); 
