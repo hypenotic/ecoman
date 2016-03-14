49688e8f09c52e96244a4f46cc0ed0c6aedbc82a
Emily Dela Cruz <delacruz.emily@gmail.com>
2016-03-14 09:43:03 -0400

    Update: fixes #8 - signup button

--- custom/themes/ecoman/template-part-signup.php
+++ custom/themes/ecoman/template-part-signup.php
@@ -22,7 +22,7 @@
 				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="your email" required>
 			    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 			    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_fb8337bb6ec003cf9e734ec8a_59b4fc2403" tabindex="-1" value=""></div>
-			    <div class="clear"><input type="submit" value="SIGN UP" name="subscribe" id="mc-embedded-subscribe" class="button white-btn" onclick="ga('send', 'event', 'outbound', 'click', 'http://ecoman.us4.list-manage.com/');"></div>
+			    <div class="clear"><input type="submit" value="sign up!" name="subscribe" id="mc-embedded-subscribe" class="button white-btn" onclick="ga('send', 'event', 'outbound', 'click', 'http://ecoman.us4.list-manage.com/');"></div>
 			    </div>
 			</form>
 			</div>
