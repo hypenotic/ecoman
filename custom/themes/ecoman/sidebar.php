da5e1eec9fa54ffc19185af62364352d3fb59c7f
Emily Dela Cruz <delacruz.emily@gmail.com>
2018-12-10 13:47:38 -0500

    Update sidebar

--- custom/themes/ecoman/sidebar.php
+++ custom/themes/ecoman/sidebar.php
@@ -21,7 +21,7 @@
 	</ul>
 
 	<span class="-serif -italic -pt1 -d-b">Social Feeds</span>
-	<ul class="-lst-none -m0 -p0 -ptb1 -flex">
+	<ul class="-lst-none -m0 -p0 -ptb1 -flex sidebar_social">
 		<li class=""><a id="facebook" target="_blank" class="btn -circle -bg-lgrey" onclick="ga('send', 'event', 'outbound', 'click', 'https://www.facebook.com/Ecomantoronto');" href="https://www.facebook.com/Ecomantoronto"><i class="fa fa-facebook"></i></a></li>
 		<li class=""><a id="twitter" onclick="ga('send', 'event', 'outbound', 'click', 'https://twitter.com/ecomandotca?lang=en');" href="https://twitter.com/ecomandotca?lang=en" target="_blank" class="btn -circle -bg-lgrey"><i class="fa fa-twitter"></i></a></li>
 		<li class=""><a id="instagram" onclick="ga('send', 'event', 'outbound', 'click', 'https://www.instagram.com/ecoman_jonas/');" href="https://www.instagram.com/ecoman_jonas/" target="_blank" class="btn -circle -bg-lgrey"><i class="fa fa-instagram"></i></a></li>
