5cdefba148c1275ef94a86ba914647e867a7313a
Emily Dela Cruz <delacruz.emily@gmail.com>
2019-01-08 11:08:34 -0500

    Update about intro type

--- custom/themes/ecoman/includes/functions/enqueue-style.php
+++ custom/themes/ecoman/includes/functions/enqueue-style.php
@@ -9,7 +9,7 @@ function my_styles() {
  	wp_enqueue_style( 'flickity' );
  	wp_register_style('awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css');
  	wp_enqueue_style( 'awesome' );
-	wp_register_style('style', get_template_directory_uri() . '/dist/css/style.css?v=190108-49fjwnw8ch2d');
+	wp_register_style('style', get_template_directory_uri() . '/dist/css/style.css?v=190108-49cj3j3doqw');
  	wp_enqueue_style( 'style' );
 }
 
