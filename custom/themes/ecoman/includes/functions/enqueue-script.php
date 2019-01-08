8f56e52f11fd39d8e57aef5f6d8a0141c58c1778
Emily Dela Cruz <delacruz.emily@gmail.com>
2019-01-08 11:49:57 -0500

    Update mobile nav

--- custom/themes/ecoman/includes/functions/enqueue-script.php
+++ custom/themes/ecoman/includes/functions/enqueue-script.php
@@ -9,9 +9,9 @@ function my_scripts() {
 	// wp_enqueue_script('jquery-ui-tabs');
 	// wp_register_script('tween', 'http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js', array('jquery'), '', true);
 	// wp_enqueue_script( 'tween' );
-	// wp_register_script('stellar', get_template_directory_uri() . '/src/js/jquery.stellar.js', array('jquery'), '', true);
+	// wp_register_script('ham', get_template_directory_uri() . '/src/js/hamburger.min.js', array('jquery'), '', true);
 	// wp_enqueue_script( 'stellar' );
-	wp_register_script('app', get_template_directory_uri() . '/dist/js/app.min.js', array('jquery'), '', true);
+	wp_register_script('app', get_template_directory_uri() . '/dist/js/app.min.js?v=190108-49jowjk39j', array('jquery'), '', true);
 	wp_enqueue_script( 'app' );
 	
 }
--- custom/themes/ecoman/template-part-nav.php
+++ custom/themes/ecoman/template-part-nav.php
@@ -1,4 +1,4 @@
-<nav class="nav -mobile">
+<nav class="nav -mobile" id="nav-fixed">
     <div class="nav_left -flex -flex-ai-c">
         <a href="<?php echo home_url(); ?>">
             <img src="<?php echo get_template_directory_uri(); ?>/dist/images/ecoman_logo_icon_black.png" alt="Ecoman logo of a hand picking up a palnt from the ground">
@@ -7,7 +7,7 @@
     </div>
 </nav>
 
-<nav class="nav">
+<nav class="nav" id="main-nav">
     <div class="nav_left -flex -flex-ai-c">
         <a class="-flex -flex-ai-c" href="<?php echo home_url(); ?>">
             <img src="<?php echo get_template_directory_uri(); ?>/dist/images/ecoman_logo_icon_black.png" alt="Ecoman logo of a hand picking up a palnt from the ground.">
@@ -24,3 +24,9 @@
         ?>
     </div>
 </nav>
+
+<button id="nav-trigger" class="hamburger hamburger--collapse" type="button">
+  <span class="hamburger-box">
+    <span class="hamburger-inner"></span>
+  </span>
+</button>
