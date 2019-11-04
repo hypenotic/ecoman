aa30aeaa253eb48ce9411bff5df08cdfb614e1b1
lucamorellato <luca.morellato@gmail.com>
2019-11-04 16:41:58 -0500

    fixed phone number

--- custom/themes/ecoman/header.php
+++ custom/themes/ecoman/header.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	<?php if (is_front_page()) { ?>
-		<meta name="description" content="Landscaping & Gardening. Ecoman helps you take a stewardship approach to your green space. 416.556.5516. Toronto." />
+		<meta name="description" content="Landscaping & Gardening. Ecoman helps you take a stewardship approach to your green space. 416.535.5565 Toronto." />
 	<?php } ?>
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 	<title><?php bloginfo('name'); ?></title>
